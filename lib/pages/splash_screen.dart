dfbbacfe8ad2c6259ba805477a172886afa72174
soheil <soheilghazimoradi@gmail.com>
2023-07-17 09:13:49 +0330

    complete ui of splash screen

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
@@ -9,6 +10,55 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    Size size = MediaQuery.of(context).size;
+    return SafeArea(
+      child: Container(
+        width: size.width,
+        height: size.height,
+        decoration: const BoxDecoration(
+          color: Color(0xff1C1F2E),
+          image: DecorationImage(
+            image: AssetImage("images/pattern1.png"),
+            repeat: ImageRepeat.repeat,
+          ),
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: size.height / 3,
+                ),
+                Image.asset(
+                  "images/logo_splash.png",
+                  width: 150,
+                ),
+                SizedBox(
+                  height: size.height / 3,
+                ),
+                const Text(
+                  "From",
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Text(
+                  "Expert Flutter",
+                  style: TextStyle(
+                    color: Color(0xff55b9f7),
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
