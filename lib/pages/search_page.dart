0f7d3053f4893ff8229ccaae33c5755ed5c6bb02
soheil <soheilghazimoradi@gmail.com>
2023-07-18 07:33:24 +0330

    complete type of stories of search_page

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -14,7 +14,39 @@ class SearchPage extends StatelessWidget {
       backgroundColor: const Color(0xff1c1f2e),
       body: Column(
         children: [
-          SearchTextField(size: size)
+          SearchTextField(size: size),
+          Container(
+            width: size.width,
+            height: 40,
+            margin: const EdgeInsets.fromLTRB(18, 20, 18, 0),
+            child: ListView.builder(
+              itemCount: 10,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                return Container(
+                  margin: const EdgeInsets.only(right: 15),
+                  decoration: const BoxDecoration(
+                    color: Color(0xff272b40),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(15),
+                    ),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.only(left: 25, right: 25, top: 10),
+                    child: Text(
+                      "Text",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                );
+              },
+              physics: const BouncingScrollPhysics(),
+            ),
+          )
         ],
       ),
     );
