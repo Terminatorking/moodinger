6e53201522110eb89e48dab5ed729fdd6c145560
soheil <soheilghazimoradi@gmail.com>
2023-07-18 08:35:12 +0330

    complete add post page

--- lib/pages/add_post_page.dart
+++ lib/pages/add_post_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
 class AddPostPage extends StatelessWidget {
   const AddPostPage({
@@ -8,6 +9,99 @@ class AddPostPage extends StatelessWidget {
   final Size size;
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: const Color(0xff1c1f2e),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    const Text(
+                      "Post",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 15,
+                    ),
+                    Image.asset(
+                      "images/icon_arrow_down.png",
+                    )
+                  ],
+                ),
+                Row(
+                  children: [
+                    const Text(
+                      "Post",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 15,
+                    ),
+                    Image.asset(
+                      "images/icon_arrow_right_box.png",
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Container(
+            width: size.width,
+            height: size.height / 1.3,
+            margin: const EdgeInsets.all(18),
+            child: GridView.custom(
+              physics: const BouncingScrollPhysics(),
+              gridDelegate: SliverQuiltedGridDelegate(
+                crossAxisCount: 3,
+                mainAxisSpacing: 4,
+                crossAxisSpacing: 4,
+                pattern: const [
+                  QuiltedGridTile(3, 3),
+                  QuiltedGridTile(1, 1),
+                  QuiltedGridTile(1, 1),
+                  QuiltedGridTile(1, 1),
+                  QuiltedGridTile(1, 1),
+                  QuiltedGridTile(1, 1),
+                  QuiltedGridTile(1, 1),
+                  QuiltedGridTile(1, 1),
+                  QuiltedGridTile(1, 1),
+                  QuiltedGridTile(1, 1),
+                ],
+              ),
+              childrenDelegate: SliverChildBuilderDelegate(
+                childCount: 40,
+                (context, index) {
+                  return Container(
+                    margin: const EdgeInsets.only(
+                        top: 8, left: 5, right: 5, bottom: 8),
+                    decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(15),
+                      ),
+                      image: DecorationImage(
+                        image: AssetImage("images/item${index % 10}.png"),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
