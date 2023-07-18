28d7494da19f8b7a8d79b2c8ccafd0aebbc29f0c
soheil <soheilghazimoradi@gmail.com>
2023-07-18 06:59:00 +0330

    complete function of navigation bar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,53 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:moodinger/pages/add_post_page.dart';
+import 'package:moodinger/pages/following_page.dart';
+import 'package:moodinger/pages/profile_page.dart';
+import 'package:moodinger/pages/search_page.dart';
 import '../widgets/post.dart';
 import '../widgets/stories.dart';
 import '../widgets/top_of_posts.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({
     super.key,
     required this.size,
   });
   final Size size;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int page = 0;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         bottomNavigationBar: BottomNavigationBar(
-          currentIndex: 0,
+          currentIndex: page,
           onTap: (value) {
-           
+            setState(
+              () {
+                page = value;
+              },
+            );
           },
           type: BottomNavigationBarType.fixed,
           backgroundColor: const Color(0xff272b40),
           items: [
             BottomNavigationBarItem(
               label: "",
-              icon: Image.asset("images/icon_active_home.png"),
+              icon: Image.asset(
+                page == 0
+                    ? "images/icon_active_home.png"
+                    : "images/icon_home.png",
+              ),
             ),
             BottomNavigationBarItem(
               label: "",
-              icon: Image.asset("images/icon_search.png"),
+              icon: Image.asset(
+                page == 1
+                    ? "images/icon_search_navigation_active.png"
+                    : "images/icon_search.png",
+              ),
             ),
             BottomNavigationBarItem(
               label: "",
-              icon: Image.asset("images/icon_add_navigation.png"),
+              icon: Image.asset(
+                page == 2
+                    ? "images/icon_add_navigation_active.png"
+                    : "images/icon_add_navigation.png",
+              ),
             ),
             BottomNavigationBarItem(
               label: "",
-              icon: Image.asset("images/icon_activity_navigation.png"),
+              icon: Image.asset(
+                page == 3
+                    ? "images/icon_activity_navigation_active.png"
+                    : "images/icon_activity_navigation.png",
+              ),
             ),
             BottomNavigationBarItem(
               label: "",
               icon: Container(
                 width: 35,
                 height: 35,
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 2,
+                    color: page == 4
+                        ? const Color(0xfff35383)
+                        : Colors.transparent,
+                  ),
+                  image: const DecorationImage(
                     image: AssetImage("images/postcover.jpg"),
                     fit: BoxFit.cover,
                   ),
-                  borderRadius: BorderRadius.all(
+                  borderRadius: const BorderRadius.all(
                     Radius.circular(15),
                   ),
                 ),
@@ -55,43 +92,54 @@ class HomePage extends StatelessWidget {
             ),
           ],
         ),
-        appBar: AppBar(
-          elevation: 0,
-          actions: [
-            Image.asset("images/icon_direct.png"),
-          ],
-          backgroundColor: Colors.transparent,
-          title: Image.asset(
-            "images/moodinger_logo.png",
-            width: 130,
-          ),
-        ),
+        appBar: page == 0
+            ? AppBar(
+                elevation: 0,
+                actions: [
+                  Image.asset("images/icon_direct.png"),
+                ],
+                backgroundColor: Colors.transparent,
+                title: Image.asset(
+                  "images/moodinger_logo.png",
+                  width: 130,
+                ),
+              )
+            : null,
         backgroundColor: const Color(0xff1c1f2e),
-        body: Column(
+        body: IndexedStack(
+          index: page,
           children: [
-            Stories(size: size),
-            const SizedBox(
-              height: 10,
+            Column(
+              children: [
+                Stories(size: widget.size),
+                const SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                  width: widget.size.width,
+                  height: widget.size.height / 1.52,
+                  child: ListView.builder(
+                    physics: const BouncingScrollPhysics(),
+                    itemCount: 5,
+                    itemBuilder: (context, index) {
+                      return Column(
+                        children: [
+                          const TopOfPosts(),
+                          Post(size: widget.size),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                ),
+              ],
             ),
-            SizedBox(
-              width: size.width,
-              height: size.height / 1.52,
-              child: ListView.builder(
-                physics: const BouncingScrollPhysics(),
-                itemCount: 5,
-                itemBuilder: (context, index) {
-                  return Column(
-                    children: [
-                      const TopOfPosts(),
-                      Post(size: size),
-                      const SizedBox(
-                        height: 30,
-                      ),
-                    ],
-                  );
-                },
-              ),
-            ),
+            SearchPage(),
+            AddPostPage(),
+            FollowingPage(),
+            ProfilePage()
           ],
         ),
       ),
