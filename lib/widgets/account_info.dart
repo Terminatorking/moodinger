d0bbfb66f2a6a37a05b45f40fd028de7f7d46d7c
soheil <soheilghazimoradi@gmail.com>
2023-07-19 06:51:04 +0330

    build button of account page

--- lib/widgets/account_info.dart
+++ lib/widgets/account_info.dart
@@ -168,10 +168,70 @@ class AccountInfo extends StatelessWidget {
                 ),
               ],
             ),
+            const SizedBox(
+              height: 20,
+            ),
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Container(width: size.width,),
-                Container()
+                SizedBox(
+                  width: size.width / 2.3,
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                        const Color(0xfff35383),
+                      ),
+                      shape: MaterialStateProperty.all(
+                        const RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(15),
+                          ),
+                        ),
+                      ),
+                      elevation: MaterialStateProperty.all(0),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      "Follow",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: size.width / 2.3,
+                  height: 50,
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(
+                        Colors.transparent,
+                      ),
+                      shape: MaterialStateProperty.all(
+                        const RoundedRectangleBorder(
+                          side: BorderSide(
+                            width: 3,
+                            color: Color(0xffc5c5c5),
+                          ),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(15),
+                          ),
+                        ),
+                      ),
+                      elevation: MaterialStateProperty.all(0),
+                    ),
+                    onPressed: () {},
+                    child: const Text(
+                      "Message",
+                      style: TextStyle(
+                        color: Color(0xffc5c5c5),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                )
               ],
             ),
           ],
